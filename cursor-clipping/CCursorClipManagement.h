dd9abb69b072de465d2dd5d30e0a10ffbd2cc758
LordOf Xen <38406718+LordOfXen@users.noreply.github.com>
2021-01-06 11:06:38 -0800

    Add files via upload

---
+++ cursor-clipping/CCursorClipManagement.h
@@ -0,0 +1,43 @@
+//===============2021===============//
+//===		  Xen & DMDr	     ===//
+//===============2021===============//
+
+#ifndef CCURSORCLIPMANAGEMENT_H
+#define CCURSORCLIPMANAGEMENT_H
+#ifdef _WIN32
+#pragma once
+#endif
+
+// This clips the cursor to the current window's client rectangle as long as it's active. This will automatically fix some bugs on VGUI too.
+class CCursorClipManagement
+{
+public:
+	CCursorClipManagement();
+	~CCursorClipManagement();
+	void Think();
+
+	static void Init();
+protected:
+	// Locks the cursor if not locked.
+	void UnlockCursor();
+	// Unlocks the cursor if locked.
+	void LockCursor(bool mouseDown = false);
+	bool GetCurrentRECT(long &left, long &top, long &right, long &bottom, bool mouseDown) ;
+	
+private:
+	void* m_HWND;
+
+	bool m_bLockState;
+	bool m_bLockStateMouseDown;
+	bool m_bToolsMode;
+	bool m_bDevMode;
+	bool m_bEnabled;
+	bool m_bLastMouseDownState;
+	bool m_bMouseDownStartedOutside;
+
+	float m_flMouseDownStartedAt;
+};
+
+extern CCursorClipManagement *g_pCursorClipManager;
+
+#endif // CCURSORCLIPMANAGEMENT_H
